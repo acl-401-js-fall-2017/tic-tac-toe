dd2b451e5c2c10eefbc610f7a1363a818ee9a91c
Maryussuperboy <martsyalis@gmail.com>
2017-11-27 12:05:20 -0800

    add board

--- src/game/Game.js
+++ src/game/Game.js
@@ -7,21 +7,24 @@ class Game extends PureComponent {
 
   componentDidMount(state) {
     console.log('we just mounted');
-    const board = [ ['b', 'b', 'b'], [ 'b', 'b', 'b'], ['b', 'b', 'b'] ];
+    const board = [ ['b', 'X', 'b'], [ 'b', 'b', 'b'], ['b', 'b', 'b'] ];
     this.props.initGame({ board, turn:'PlayerX', finished: false }) ;
   }
 
   render(){
+
     return (
       <div>
         <StyledDiv>
-          { this.props.game.board.map(row => {
-            console.log('row is', row);
-            return row.map(column => {
-              console.log('column is', column);
-              return <div style={{ border: '1px solid black' }}>{column}</div>;
-            });
-          })}
+          { 
+            this.props.game.map(game => {
+              return game.board.map(column => {
+                return column.map(block =>{
+                  return <div key={block} style={{ border: '1px solid black' }}>{block}</div>;
+                });
+              });
+            })
+          }
         </StyledDiv>
       </div>
     );
--- src/game/reducer.js
+++ src/game/reducer.js
@@ -1,10 +1,13 @@
 import * as actions from './constants';
 
-export default function Game(state = { board:[ ['a','a','a'] ,['a','a','a'], ['a','a','a'] ], turn: 'PlayerX', finished: false }, { type, payload }) {
+export default function Game(state = [], { type, payload }) {
   switch (type) {
   case actions.INITIATE_GAME:
     console.log('we are in the right case with payload of', payload);
-    return Object.assign({}, state, payload);
+    return [
+      ...state,
+      payload
+    ];
   default:
     console.log('we shouldnt be here');
     return state; 
