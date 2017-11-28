ab2aee074edd145a2c2636a464b07ea8ccc83235
Zac-L <Zachlichens@gmail.com>
2017-11-28 09:46:40 -0800

    created actions for each constants

--- src/game/actions.js
+++ src/game/actions.js
@@ -1,9 +1,32 @@
 import * as actions from './constants';
 
-export function initGame(game){
-  console.log('we are in initGame and game is', game);
-  return {
-    type: actions.INITIATE_GAME,
-    payload: game
+export function takeTurn(index) {
+  return (dispatch, getState) => {
+
+    const { turn } = getState();
+    const token = turn === 'PlayerX' ? 'X' : 'O';
+
+    dispatch({
+      type: actions.TAKE_TURN,
+      payload: { index, token }
+    });
+
+    const { board } = getState();
+
+    if(gameWon()) {
+      dispatch({
+        type: actions.WIN_GAME,
+        payload: turn
+      });
+    }
+    
+    if(board.filter(square => square !== 'a').length === 9) {
+      dispatch({
+        type: actions.TIE_GAME
+      });
+    }
+
   };
 }
+
+const gameWon = () => false;
