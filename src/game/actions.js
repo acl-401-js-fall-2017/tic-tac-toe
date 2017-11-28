5401700e6f8ca7070012b5e2a7c1dcbe2ecb5af9
Zac-L <Zachlichens@gmail.com>
2017-11-28 11:20:40 -0800

    refactored switch case

--- src/game/actions.js
+++ src/game/actions.js
@@ -30,34 +30,17 @@ export function takeTurn(index) {
 }
 
 const gameWon = (board) => {
-  console.log('we are checking win for board:', board);
-  console.log( board[0] === board[1] && board[0] === board[2]);
   switch(true) {
-  case board[0] === board[1] && board[0] === board[2] :{
-    console.log('win at first row');
+  case board[0] === board[1] && board[0] === board[2]:
+    console.log('first row win');
+  case board[0] && board[0] === board[3] && board[0] === board[6]:
+  case board[3] && board[3] === board[4] && board[3] === board[5]:
+  case board[1] && board[1] === board[4] && board[1] === board[7]:
+  case board[6] && board[6] === board[7] && board[6] === board[8]:
+  case board[2] && board[2] === board[5] && board[2] === board[8]:
+  case board[0] && board[0] === board[4] && board[0] === board[8]:
+  case board[2] && board[2] === board[4] && board[2] === board[6]:
     return true;
-  }
-  case board[0] && board[0] === board[3] && board[0] === board[6] :{
-    return true;
-  }
-  case board[3] && board[3] === board[4] && board[3] === board[5] :{
-    return true;
-  }
-  case board[1] && board[1] === board[4] && board[1] === board[7] :{
-    return true;
-  }
-  case board[6] && board[6] === board[7] && board[6] === board[8] :{
-    return true;
-  }
-  case board[2] && board[2] === board[5] && board[2] === board[8] :{
-    return true;
-  }
-  case board[0] && board[0] === board[4] && board[0] === board[8] :{
-    return true;
-  }
-  case board[2] && board[2] === board[4] && board[2] === board[6] :{
-    return true;
-  }
   default: {
     return false;
   }
