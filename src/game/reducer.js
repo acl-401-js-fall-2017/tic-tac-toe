3b09501d398248457de1b44dfaa756d19dca06b8
Zac-L <Zachlichens@gmail.com>
2017-11-28 09:31:08 -0800

    added functions for take turn, board and finished game for reducers

--- src/game/reducer.js
+++ src/game/reducer.js
@@ -13,3 +13,42 @@ export default function Game(state = [], { type, payload }) {
     return state; 
   }
 }
+
+const getEmptyBoard = () => {
+  return [
+    'a','a','a',
+    'a','a','a',
+    'a','a','a'
+  ];
+};
+
+export function board(state = getEmptyBoard(), { type, payload }) {
+  switch (type) {
+  case actions.TAKE_TURN: {
+    const { index, token } = payload;
+    return state.map((square, i) => i === index ? token : square);
+  }
+  default:
+    return state;
+  }
+}
+
+export function turn(state = 'PlayerX', { type, payload }) {
+  switch(type) {
+  case actions.TAKE_TURN: {
+    return state === 'PlayerX' ? 'PlayerO' : 'PlayerX';
+  }
+  default:
+    return state;
+  }
+}
+
+export function finished(state = false, { type, payload }) {
+  switch(type) {
+  case actions.WIN_GAME:
+  case actions.TIE_GAME:
+    return true;
+  default:
+    return state;
+  }
+}
