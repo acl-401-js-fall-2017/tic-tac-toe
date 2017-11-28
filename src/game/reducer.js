45ff61c462bea99905592e25ba8473e385bde076
Zac-L <Zachlichens@gmail.com>
2017-11-28 09:31:55 -0800

    removed unnneeded function

--- src/game/reducer.js
+++ src/game/reducer.js
@@ -1,19 +1,5 @@
 import * as actions from './constants';
 
-export default function Game(state = [], { type, payload }) {
-  switch (type) {
-  case actions.INITIATE_GAME:
-    console.log('we are in the right case with payload of', payload);
-    return [
-      ...state,
-      payload
-    ];
-  default:
-    console.log('we shouldnt be here');
-    return state; 
-  }
-}
-
 const getEmptyBoard = () => {
   return [
     'a','a','a',
